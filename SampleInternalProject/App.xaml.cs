7e9de551bce0dbb6c10c9a9da786ffcacffd58ed
Abdülkadir Çakır <afcsoft@outlook.com>
2024-08-06 08:34:59 +0300

    Add project files.

---
+++ SampleInternalProject/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SampleInternalProject
+{
+  public partial class App : Application
+  {
+    [DllImport("__Internal",EntryPoint ="main")]
+    public static extern int main();
+    public App()
+    {
+      main();
+      InitializeComponent();
+
+      MainPage = new AppShell();
+    }
+  }
+}
